04a9ebb6324ca8593b95e162ec703e55b10d398d
slime1212 <81129402+slime1212@users.noreply.github.com>
2021-06-19 10:24:52 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+canvas = document.getElementById("mycanvas");
+
+ctx = canvas.getContext("2d");
+
+color = "black";
+
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 3;
+ctx.arc(300, 300, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e) {
+    color = document.getElementById("color").value;
+    console.log(color);
+    mouse_x = e.clientX - canvas.offsetLeft;
+    mouse_y = e.clientY - canvas.offsetTop;
+    console.log("X: " + mouse_x + ", Y: " + mouse_y);
+    circle(mouse_x, mouse_y);
+};
+
+function circle(mouse_x, mouse_y) {
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 3;
+    ctx.arc(mouse_x, mouse_y, 40, 0, 2 * Math.PI);
+    ctx.stroke()
+};
+
+function clear() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
